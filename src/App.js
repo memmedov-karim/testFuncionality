740d3fc8d32e88f1373662d70f007d3a09bd415b
Shikhkarim <sixkerimmemmedov2001@gmail.com>
2023-04-03 17:50:16 +0400

    This is second version!...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './components/Hello.js';
 function App() {
   return (
     <div className="App">
       <h1>This is First version!...</h1>
+      <h2>This is second version!...</h2>
+      <Hello />
     </div>
   );
 }
